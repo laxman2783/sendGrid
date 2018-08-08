7c3ed202c70f15f2cf28aa9134fadd3503da5a6f
Laxman Thota <laxmant.thota@gmail.com>
2018-08-08 06:45:28 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+const sgMail = require('@sendgrid/mail');
+http.createServer(function (req, res) {
+    console.log("Running");
+     res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  
+  //Open a file on the server and return it's content:
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey("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");
+const msg = {
+  to: 'laxmant.thota@gmail.com',
+  from: 'laxmant.thota@gmail.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+console.log(sgMail.send(msg));
+res.end();
+}).listen(8080);
